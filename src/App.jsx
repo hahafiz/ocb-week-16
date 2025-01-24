7c870f42b088f042a2626cb8eb7daa198673eac9
hahafiz <hafizqamarul19@gmail.com>
2025-01-25 00:37:43 +0800

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Card from "./components/Card";
 
+const user = {
+  name: "Meow",
+  avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+  city: "Seattle",
+  follower: 1234,
+  following: 4321,
+  education: {
+    primary: "SK Megah Ria",
+    secondary: "SMK Seri Kota Puteri 2",
+  },
+};
+
 function App() {
   return (
     <>
-      <Card name="Jeff" />
+      <Card user={user} />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,14 +5,21 @@ import PropTypes from "prop-types";
 const Card = (props) => {
   return (
     <div>
-      {props.name}
-      <p>Hello</p>
+      <p>Hello, {props.user.name}</p>
+      <img src={props.user.avatar} alt={props.user.name} />
     </div>
   );
 };
 
 Card.propTypes = {
-  name: PropTypes.string,
+  // name: PropTypes.string,
+  user: PropTypes.shape({
+    name: PropTypes.string,
+    avatar: PropTypes.string,
+    city: PropTypes.string,
+    follower: PropTypes.number,
+    following: PropTypes.number,
+  }),
 };
 
 export default Card;
