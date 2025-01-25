a5ff38c915f118a221028316be4a5d4002b2e8a9
hahafiz <hafizqamarul19@gmail.com>
2025-01-25 16:29:35 +0800

    use map function to create Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,60 @@
 import Card from "./components/Card";
 
-const user = {
-  name: "Meow",
-  avatar: "https://i.imgur.com/yXOvdOSs.jpg",
-  city: "Seattle",
-  follower: 1234,
-  following: 4321,
-  education: {
-    primary: "SK Megah Ria",
-    secondary: "SMK Seri Kota Puteri 2",
+// const user = {
+//   name: "Meow",
+//   avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+//   city: "Seattle",
+//   follower: 1234,
+//   following: 4321,
+//   education: {
+//     primary: "SK Megah Ria",
+//     secondary: "SMK Seri Kota Puteri 2",
+//   },
+// };
+
+const users = [
+  {
+    name: "Meow",
+    avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+    city: "Seattle",
+    follower: 1234,
+    following: 4321,
+    education: {
+      primary: "SK Megah Ria",
+      secondary: "SMK Seri Kota Puteri 2",
+    },
   },
-};
+  {
+    name: "iamapanda",
+    avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+    city: "Seattle",
+    follower: 1234,
+    following: 4321,
+    education: {
+      primary: "SK Megah Ria",
+      secondary: "SMK Seri Kota Puteri 2",
+    },
+  },
+  {
+    name: "roarrr",
+    avatar: "https://i.imgur.com/yXOvdOSs.jpg",
+    city: "Seattle",
+    follower: 1234,
+    following: 4321,
+    education: {
+      primary: "SK Megah Ria",
+      secondary: "SMK Seri Kota Puteri 2",
+    },
+  },
+];
 
 function App() {
   return (
     <>
-      <Card user={user} />
+      {/* <Card user={user} /> */}
+      {users.map((user) => {
+        return <Card user={user} />;
+      })}
     </>
   );
 }
