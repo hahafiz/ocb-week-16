915e8e89d4b23c7a2619eef1c929b6f3380b65e1
hahafiz <hafizqamarul19@gmail.com>
2025-01-26 21:35:55 +0800

    done part 1

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,10 +1,18 @@
 // import PropTypes from 'prop-types'
 import { useState } from "react";
+import Display from "./Display";
 
 const Count = () => {
   const [counter, setCounter] = useState(0);
 
-  return <div>Count</div>;
+  return (
+    <div>
+      <Display displayNumber={counter} />
+      <button onClick={() => setCounter((previousValue) => previousValue + 1)}>
+        Increase
+      </button>
+    </div>
+  );
 };
 
 // Count.propTypes = {}
---
+++ src/components/Display.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Display = (props) => {
+  return <div>Counter: {props.displayNumber}</div>;
+};
+
+Display.propTypes = {
+  displayNumber: PropTypes.number,
+};
+
+export default Display;
