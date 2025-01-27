67ba0c1fa43f60a4c6701f27dc7b1dda5a861863
hahafiz <hafizqamarul19@gmail.com>
2025-01-27 20:56:18 +0800

    adding css to component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,27 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+import "./Counter.css";
 
 const Counter = (props) => {
-  return <div>Hi {props.name}!</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((previousCount) => {
+      return previousCount + 1;
+    });
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "16px",
+      }}
+    >
+      <p className="title">Hi {props.name}!</p>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Add 1</button>
+    </div>
+  );
 };
 
 Counter.propTypes = {
