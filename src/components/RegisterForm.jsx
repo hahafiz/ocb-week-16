801cb05a1563e06a048e3041d7fe597de7712d6f
hahafiz <hafizqamarul19@gmail.com>
2025-01-28 21:44:29 +0800

    form handleSubmit event

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,14 +4,26 @@ import { useState } from "react";
 const RegisterForm = () => {
   // fieldset - component to group inputs
   const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("username >>>>>", username);
+    console.log("age >>>>>", age);
+    console.log("password >>>>>", password);
+
+    // TODO: Submit to API
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <fieldset>
         <label htmlFor="username">Username</label>
         <input
           id="username"
           name="username"
+          value={username}
           onChange={(event) => {
             // console.log(event.target.value);
             setUsername(event.target.value);
@@ -20,11 +32,27 @@ const RegisterForm = () => {
       </fieldset>
       <fieldset>
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" name="age" />
+        <input
+          type="number"
+          id="age"
+          name="age"
+          value={age}
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
       </fieldset>
       <fieldset>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
       </fieldset>
       <button type="submit">Submit</button>
     </form>
